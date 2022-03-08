471c127682169ed8491f91f9e48d9429ea4f47ff
Kuba <Kuba@DESKTOP-1CGGKO9>
2022-03-08 17:57:11 +0100

    Add project files.

---
+++ ConsoleApp2/Problem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Problem
+    {
+        public List<int> numbers;
+
+        public Problem(int n, int seed)
+        {
+            Random r = new Random(seed);
+            numbers = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Add(r.Next(50));
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = string.Join(" ", numbers.ToArray());
+            return str;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the number");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the seed");
+            int seed = int.Parse(Console.ReadLine());
+            Problem problem = new Problem(n, seed);                       
+        }
+    }
+}
